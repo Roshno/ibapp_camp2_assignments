8a2ace116365d7f5e99aac83acac12d86051c48b
Roshni <roshni.s@inapp.com>
2022-07-19 13:02:36 +0530

    assignment completed

--- python/newfile.py
+++ python/newfile.py
@@ -1,12 +1,13 @@
 
 import sys
 from tkinter import SE
+from tkinter.tix import DisplayStyle
 from tkinter.ttk import Separator 
 
-
+"""
 print ("hello world")
 #JUST PRINTING THE PYTHON VERSION
-"""just a
+just a
 multiline comment
 print (sys.version)
 
@@ -199,7 +200,7 @@ NumList.sort()
 
 print("Element After Sorting List in Ascending Order is : ", NumList)
 
-"""
+
 #1. sort list in ascending order
 numbers = [1, 3, 4, 2]
 # Sorting list of Integers in ascending
@@ -270,4 +271,281 @@ string = 'They ate 6 apples and 10 bananas'
 string = re.sub('6', 'six', string)
 string = re.sub('10', 'ten', string)
 print (string)
-List = [1,2]
+
+
+#Tuples in Python
+months = ("Jan","Feb","March")
+print(months[0])
+print(months[-1])
+#months[0] = "test" will not work
+
+#tuples methods in python
+print(len(months))
+print("Jan" in months)
+print("January" in months)
+#del months
+print(months)
+print(months+months)
+print(months*3)
+
+
+#dictionary in python
+#dictionary declaration
+#method1
+myStudents = {"Abhi":30, "Sibi":28, "Subi":"not updated"}
+print(myStudents)
+#method2
+myStudents = dict(Abhi=30, Sibi=28, Subi="not updated")
+print(myStudents)
+#method3
+myStudents = dict({"Abhi":30, "Sibi":28, "Subi":"not updated"})
+print(myStudents)
+print(myStudents["Sibi"])
+
+#Dictionary methods
+print(myStudents.get("Subi"))#will return corressponding value
+print(myStudents.items()) #will return dict items as array of tuples
+print(myStudents.keys()) #return keys of the dict
+print(myStudents.values()) #return values of the dict
+print("Abhi" in myStudents) #check if value is present
+print(30 in myStudents.values()) #check if value is present
+print(len(myStudents))
+
+myStudents2 = {"Abhi": 31, "Binu": 26}
+myStudents.update(myStudents2)#join together dict by overwriting duplicate keys
+print(myStudents)
+
+myStudents.clear() #clear all values inside dict
+print(myStudents)
+
+del myStudents #delete dict along with variable
+print(myStudents)
+
+
+#Set in python
+
+#method1
+months = {"Jan","Feb","March"}
+#method2 
+months = set(["Jan","Feb","March"])
+
+print(months)
+print(type(months))
+
+#looping thriugh elements in a set
+for i in months:
+    print(i)
+
+#declare an empty set
+days = set()
+#add values to set
+days.add("Mon") #insert single values
+days.add("Tue")
+days.add("Wed")
+#looping thriugh elements in a set
+for i in days:
+    print(i)
+
+days.update(["Thur","Fri"]) #insert multiple items
+#looping thriugh elements in a set
+for i in days:
+    print(i)
+
+#remove items from the set
+#using discard() method, will remove item, not display error if item does not exist
+days.discard("Thur")
+for i in days:
+    print(i)
+
+#using remove() method, will remove item, will display error if item does not exist
+days.remove("Thur")
+#loop through items in set
+for day in days:
+    print(day)
+
+days.clear()
+print("cleared the set")
+for day in days:
+    print(day)
+  
+
+#set operations
+months1 = {"Jan","Feb","Mar"}
+months2 = set(["Mar","Apr","May"])
+#union operation
+months4 = months1 | months2
+print(months4)
+for month in months4:
+    print(month)
+
+#intersection operation
+months4 = months1 & months2
+print(months4)
+
+#intersection update
+months1 = {"Jan","Feb","Mar"}
+months2 = {"Mar","Apr","May","Feb"}
+months3 = {"Feb","Mar","Jun","Jul"}
+#months1.intersection_update(months2,months3)
+#print(months1)
+
+#difference operation
+months4 = months1 - months2
+print(months4)
+
+#symmetric difference 
+#will retain all elements of set excluding the common ones
+months4 = months1 ^ months2
+months4 = months1.symmetric_difference(months2)
+print(months4)
+
+#Set comparisons operation
+months1 = {"Jan","Feb","Mar","Apr","May","Jun"}
+months2 = {"Mar","Apr","May","Feb"}
+months3 = {"Feb","Mar","Jun","Jul"}
+#checking if months1 is a superset of months2
+months1 > months2
+print(months1 >months2)
+print(months1 <months2)
+#check if two sets are equal
+print(months2 == months3)
+#check if two sets are equal
+print(months1 >= months2)
+#check if two sets are equal as well as month2 is a superset of month1
+print(months1 <= months2)
+
+#frozen set
+months4frozen = frozenset(["Nov","Dec"]) #immutable set
+print(type(months4frozen))
+print(months4frozen)
+
+months4frozen.add("oct") #frozen set has no attribute
+
+#input and output functions in python
+studName = input("Please enter you name: ")
+studAge = input("Please enter you age: ")
+print(studName)
+print(type(studName))
+print(studAge)
+print(type(studAge))
+
+#variations of print statement to include variables
+print("The student name is",studName, "and the age is",studAge)
+print("The student name is %s and the age is %s" %(studName,studAge))
+print("The student name is {} and the age is {}" .format(studName,studAge))
+
+#print in multiple lines
+print('''Hello World
+How are you''')
+#print a new line
+print("Hello World\nHow are you")
+print("This is a backslash\\")
+print('I am 5\' 5\" tall')
+
+#control flow ststement
+#conditional statements
+#if condition
+userInputNo = input('Enter either 1 or 2:')
+if(userInputNo == "1"):
+    print("You entered 1")
+    print("And you are number 1")
+elif(userInputNo == "2"):
+    print("You entered 2")
+    print("Runner up. Keep it up!")
+else:
+    print("you did not enter 1 or 2")
+
+#inline if statement
+B=12
+A=12 if B==10 else 13
+print(A)
+
+print("B is ten" if B==10 else "B is not 10")
+
+#oython match case statement example
+#define a function
+
+def http_status(status):
+  match status:
+    case 400:
+      return "Bad Request"
+    case 404:
+      return "Page not found"
+    case _:
+      return "Unknown error occured"
+#calling the function inside print statement
+print(http_status(404))
+"""
+
+userInputMonth = input('Enter DOB month number:')
+if(userInputMonth == "1"):
+    print("Garnet")
+    print("People born in January are bold and alert")
+elif(userInputMonth == "2"):
+    print("Amethyst")
+    print("People born in February are lucky and loyal")
+elif(userInputMonth == "3"):
+    print("Aquamarine")
+    print("People born in March are naughty and gentle")
+elif(userInputMonth == "4"):
+    print("Diamond")
+    print("People born in April are caring and strong")
+elif(userInputMonth == "5"):
+    print("Emarld")
+    print("People born in May are kucky and practical")
+elif(userInputMonth == "6"):
+    print("Alexandrite")
+    print("People born in June are romantic and curious")
+elif(userInputMonth == "7"):
+    print("Ruby")
+    print("People born in July are adventurous and honest")
+elif(userInputMonth == "8"):
+    print("Peridot")
+    print("People born in August are active and hardworking")
+elif(userInputMonth == "9"):
+    print("Sapphire")
+    print("People born in September are romantic and pretty")
+elif(userInputMonth == "10"):
+    print("Toumaline")
+    print("People born in October are stylish and friendly")
+elif(userInputMonth == "11"):
+    print("Citrine")
+    print("People born in November are nice and creative")
+elif(userInputMonth == "12"):
+    print("Zircon")
+    print("People born in December are nice and creative")
+else:
+    print("invalid number")
+
+def http_status(status):
+  match status:
+    case 1:
+      return "Garnet\nPeople born in January are bold and alert"
+    case 2:
+      return "Amethyst\nPeople born in February are lucky and loyal"
+    case 3:
+      return "Aquamarine\nPeople born in March are naughty and gentle"
+    case 4:
+      return "Diamond\nPeople born in April are caring and strong"
+    case 5:
+      return "Emarld\nPeople born in May are kucky and practical"
+    case 6:
+      return "Alexandrite\nPeople born in June are romantic and curious"
+    case 7:
+      return "Ruby\nPeople born in July are adventurous and honest"
+    case 8:
+      return "Peridot\nPeople born in August are active and hardworking"
+    case 9:
+      return "Sapphire\nPeople born in September are romantic and pretty"
+    case 10:
+      return "Toumaline\nPeople born in October are stylish and friendly"
+    case 11:
+      return "Citrine\nPeople born in November are nice and creative"
+    case 12:
+      return "Zircon\nPeople born in December are nice and creative"
+    case _:
+      return "Unknown error occured"
+  
+
+
+
