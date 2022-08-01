62a6e07f0a1a568cfa9bd3781e3abefcbe54936c
Roshni <roshni.s@inapp.com>
2022-08-01 17:11:01 +0530

    Assignment done

--- python/floydtriangle.py
+++ python/floydtriangle.py
@@ -6,6 +6,6 @@ number = 1
 print("Floyd's Triangle") 
 for i in range(1, rows + 1):
     for j in range(1, i + 1):        
-        print(number, end = '  ')
+        print(number, end = '\t')
         number = number + 1
     print()
